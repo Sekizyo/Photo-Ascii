595717150f02883c2a2e8a1ef286bc89f9780bfe
Joseph <mateuszpieczulis123@gmail.com>
2020-11-19 20:05:12 +0100

    comments

--- main.py
+++ main.py
@@ -3,41 +3,43 @@ from PIL import Image, ImageDraw, ImageFont
 
 img_name = "" # Image name
 
-scale = 4 # Image scale
-probe = 8 # Skip pixels
-shift = 4 # Move line to right
-font_size = 12 # Font size
+scale = 1 # Image scale
+sample = 64 # Skip pixels
+font_size = 40 # Font size
+shift = 2 # Move characters to right
 
-colored = True # Colored or Mono
-extended_range = False # User non typicall letters
+colored = False # Colored or Mono
+extended_range = False # Extended ASCII characters
+reverce_basic_colors = False # Set background black and characters white (useful with mono images)
 
 # Read args
 for x in range(len(sys.argv)):
-    if sys.argv[x] == "-help":
-        print("Usage: <scale> <probe> <font size> <shift> <colored> <extended range>")
+    arg = sys.argv[x]
+    if arg == "-help":
+        print("Usage: <scale> <sample> <font size> <shift>")
+        print("-c   Colored version\n")
+        print("-e   Extended ASCII characters\n")
+        print("-r   Set background white and characters white (useful with mono images)\n")
+        print("More info: https://github.com/Sekyzio/Photo-Ascii/README.md\n")
         exit()
+
+    elif arg == "-c":
+        colored = True
+    elif arg == "-e":
+        extended_range = True
+    elif arg == "-r":
+        reverce_basic_colors = True
 
     if x == 1:
         img_name = sys.argv[x]
     elif x == 2:
         scale = float(sys.argv[x])
     elif x == 3:
-        probe = int(sys.argv[x])
+        sample = int(sys.argv[x])
     elif x == 4:
         font_size = int(sys.argv[x])
     elif x == 5:
         shift = int(sys.argv[x])
-    elif x == 6:
-        if sys.argv[x] == "true":
-            colored = True
-        elif sys.argv[x] == "false":
-            colored = False
-    elif x == 7:
-        if sys.argv[x] == "true":
-            extended_range = True
-        elif sys.argv[x] == "false":
-            extended_range = False
-
 
 if len(sys.argv) < 5:
     print("Too few arguments")
@@ -53,7 +55,7 @@ move_right = 0 # Shift value
 # Get values for every pixel
 for y in range(size[1]):
     for x in range(size[0]):
-        if move_right == probe:
+        if move_right == sample:
             color = img.getpixel((x,y))
             data.append(((x, y), color))
             move_right = 0
@@ -65,8 +67,16 @@ for y in range(size[1]):
     else:
         move_right -= shift
 
+
+if reverce_basic_colors:
+    bg_color = (255, 255, 255, 0) # Background color
+    ch_color = (0, 0, 0, 0) # Character color
+else:
+    bg_color = (0, 0, 0, 0)
+    ch_color = (255, 255, 255, 255)
+
 # Create new img
-new_img = Image.new('RGB', (int(size[0]*scale), int(size[1]*scale)), (0,0,0,0))
+new_img = Image.new('RGB', (int(size[0]*scale), int(size[1]*scale)), bg_color)
 d = ImageDraw.Draw(new_img)
 for i in range(len(data)):
     values = data[i]
@@ -86,9 +96,9 @@ for i in range(len(data)):
             color_avg = 126
 
     if colored: # Colored or Mono
-        d.text((x*scale,y*scale), chr(color_avg), font=fnt, fill=(colors[0], colors[1], colors[2],255))
+        d.text((x*scale,y*scale), chr(color_avg), font=fnt, fill=(colors[0], colors[1], colors[2], 255))
     else:
-        d.text((x*scale,y*scale), chr(color_avg), font=fnt, fill=(255, 255, 255, 255))
+        d.text((x*scale,y*scale), chr(color_avg), font=fnt, fill=(ch_color))
 
 # Save new img
 new_img.save("output.png")
